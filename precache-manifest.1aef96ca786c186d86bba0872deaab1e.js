7090b93f2ae16c66e71f37d8a349b4233bbbd9ce
gegitz <eduardo.gegitz@gmail.com>
2020-10-23 14:54:23 -0300

    Updates

---
+++ precache-manifest.1aef96ca786c186d86bba0872deaab1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88ed4a70093f11c25245f7c81130674e",
+    "url": "/controleAprovacao/index.html"
+  },
+  {
+    "revision": "f594422ac1752e34d2a6",
+    "url": "/controleAprovacao/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "836c89cd49d0ef187a53",
+    "url": "/controleAprovacao/static/css/main.639dab70.chunk.css"
+  },
+  {
+    "revision": "f594422ac1752e34d2a6",
+    "url": "/controleAprovacao/static/js/2.827106be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/controleAprovacao/static/js/2.827106be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "836c89cd49d0ef187a53",
+    "url": "/controleAprovacao/static/js/main.673b3ab2.chunk.js"
+  },
+  {
+    "revision": "eb9ac003af804fad810b",
+    "url": "/controleAprovacao/static/js/runtime-main.fa3b705e.js"
+  }
+]);
